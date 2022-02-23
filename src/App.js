fcb79ad419b64d496b481a48c9f5d53610bf3885
Prabesh <prabesh.bista369@gmail.com>
2022-02-23 10:07:50 +0545

    styling the components of perosnal website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Components/header";
 
 function App() {
   return (
    <div>
-
+    <Header/>
    </div>
   );
 }
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import classes from "./header.module.css";
 
 function Header(){
     return(
+        <div className={classes.header}>
         <div>
-
+            <h1>P.</h1>
+        </div>
+        <div className={classes.div2}>
+            <h4>Github</h4>
+            <h4>About</h4>
+        </div>
+        <button>Say Hello!</button>
         </div>
     );
 }
