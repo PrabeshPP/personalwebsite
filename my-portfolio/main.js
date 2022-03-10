c6ba751300dc1850b9ba9db30c341c8caa59d020
Prabesh <prabesh.bista369@gmail.com>
2022-03-10 15:19:02 +0545

    testing differnet 3D UI

--- my-portfolio/main.js
+++ my-portfolio/main.js
@@ -3,6 +3,34 @@ import * as Three from "three";
 const scene=new Three.Scene();
 const camera=new Three.PerspectiveCamera(75,innerWidth/innerHeight,0.1,1000);
 const renderer=new Three.WebGLRenderer();
-console.log(scene);
-console.log(camera);
-console.log(renderer);
+
+// adding the dom
+
+renderer.setSize(innerWidth,innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const boxGeometry=new Three.BoxGeometry(1,1,1);
+const material=new Three.MeshBasicMaterial({color:0xFFA07A})
+
+
+const mesh=new Three.Mesh(boxGeometry,material);
+
+
+scene.add(mesh);
+camera.position.z=5;
+
+function animate(){
+  renderer.render(scene,camera);
+  mesh.rotation.x+=0.01;
+  mesh.rotation.y+=0.01
+  requestAnimationFrame(animate);
+
+
+  }
+ 
+
+
+
+animate();
+
