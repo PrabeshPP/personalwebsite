a67d469686cf648d4c1138f4f651a6ace8c836d5
Prabesh <prabesh.bista369@gmail.com>
2022-04-09 17:09:59 +0545

    techStack added

--- src/components/Layout/Part2/TechStack/TechStack.js
+++ src/components/Layout/Part2/TechStack/TechStack.js
@@ -1,9 +1,14 @@
-import {DiHtml5} from "react-icons/di";
+import {DiHtml5,DiJava,DiCss3,DiDart,DiJavascript,DiReact} from "react-icons/di";
 
 const TechStack=()=>{
     return(
         <div>
+            <DiJava/>
+            <DiDart/>
+            <DiJavascript/>
             <DiHtml5/>
+            <DiCss3/>
+            <DiReact/>
         </div>
     )
 }
