f6d0b8f090527e30f292cab2dff50a87aa1d582b
Prabesh <prabesh.bista369@gmail.com>
2022-04-09 08:32:14 +0545

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import Header from './components/Layout/Header';
+import Header from './components/Layout/Header/Header';
 import classes from './App.module.css';
-import DescriptionBox from './components/Layout/Description';
+
 
 function App() {
   return (
