a11b7ee5967e2c80fc1820b3227e59207bfcb023
Prabesh <prabesh.bista369@gmail.com>
2022-04-10 09:56:39 +0545

    Part2

--- src/components/Layout/Part2/OverView.js
+++
@@ -1,7 +0,0 @@
-const OverView=()=>{
-
-
-    
-}
-
-export default OverView;
---
+++ src/components/Layout/Part2/TechStack/OverView.js
@@ -0,0 +1,22 @@
+import classes from "./OverView.module.css";
+const OverView=()=>{
+    return <div className={classes.container}>
+        <div className={classes.box}>
+        <div className={classes.box1}>
+            <span>Developer and Trader,specialized in  Mobile App,Web App ,DSA and BlockChain Developer.</span>
+        </div>
+        <div className={classes.box2}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        </div>
+
+    </div>
+
+
+
+}
+
+export default OverView;
