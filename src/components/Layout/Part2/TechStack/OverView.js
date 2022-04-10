a36176ab3dd0a26b6bee502c6f93859bccf4d8fe
Prabesh <prabesh.bista369@gmail.com>
2022-04-10 10:43:10 +0545

    Overview Completed

--- src/components/Layout/Part2/TechStack/OverView.js
+++ src/components/Layout/Part2/TechStack/OverView.js
@@ -1,22 +1,51 @@
 import classes from "./OverView.module.css";
-const OverView=()=>{
-    return <div className={classes.container}>
-        <div className={classes.box}>
+import { HiDesktopComputer,HiDeviceMobile } from "react-icons/hi";
+import {RiReactjsFill} from "react-icons/ri";
+import {FaEthereum} from "react-icons/fa";
+const OverView = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.box}>
         <div className={classes.box1}>
-            <span>Developer and Trader,specialized in  Mobile App,Web App ,DSA and BlockChain Developer.</span>
+          <span>
+            Developer and Trader,specialized in Mobile App,Web App ,DSA and
+            BlockChain .
+          </span>
         </div>
         <div className={classes.box2}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-        </div>
+          <div className={classes.frontEnd}>
+            <HiDesktopComputer className={classes.desktop} />
+            <div className={classes.text}>
+            <span>Front-End Developer</span>
+            </div>
+          </div>
+          <div className={classes.mobileApp}>
+            <HiDeviceMobile className={classes.mobile}/>
+            <div className={classes.text}>
+            <span>Mobile App Developer</span>
+            </div>
 
+          </div>
+          <div className={classes.Algo}>
+            <RiReactjsFill className={classes.algo}/>
+            <div className={classes.text}>
+            <span>Data Structures and Algorithms</span>
+            </div>
+
+          </div>
+          <div className={classes.blockChain}>
+            <FaEthereum className={classes.eth}/>
+            <div className={classes.text}>
+            <span>BlockChain App Developer</span>
+            </div>
+
+          </div>
+          
+          
+        </div>
+      </div>
     </div>
-
-
-
-}
+  );
+};
 
 export default OverView;
