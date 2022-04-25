f1b7be3cd7553046abeee9bb649457c99ba0894f
Prabesh <prabesh.bista369@gmail.com>
2022-04-25 13:00:03 +0545

    added react Router

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,6 +1,10 @@
 import classes from "./Header.module.css";
 import ProgrammerIcon from "../../Icon_Layout/Programmer_Icon";
+
 const Header = () => {
+
+  
+
   return (
     <div className={classes.header}>
       <span className={classes.progIcon}>
@@ -8,7 +12,7 @@ const Header = () => {
       </span>
       <div className={classes.box2}>
         <ul>
-          <li>Home</li>
+          <li onClick={scrollToSection}>Home</li>
           <li>Services</li>
           <li>About</li>
           
