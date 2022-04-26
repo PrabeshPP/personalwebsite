447e11ce30815fff10a063938ba6b64f01302255
Prabesh <prabesh.bista369@gmail.com>
2022-04-26 12:54:00 +0545

    .

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const container=document.getElementById('root');
 const root=createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   
 );
 
