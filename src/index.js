13714b74a6ddb5af2dcb5fb26ee71cbabd710fd9
Prabesh <prabesh.bista369@gmail.com>
2022-04-26 12:58:26 +0545

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const container=document.getElementById('root');
 const root=createRoot(container);
