98069a48cb9ef1d54bd4fce882bab7f9acc71e47
Prabesh <prabesh.bista369@gmail.com>
2022-04-26 12:18:59 +0545

    final touch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import Part1 from './components/Layout/Part1/Part1_layout';
+
 import {Routes,Route,Navigate} from "react-router-dom";
-import Part2 from "./components/Layout/Part2/Part2_layout"
 import Layout from './components/Layout/Layout';
-import Part3 from './components/Layout/Part3/Part3';
+import React,{Suspense} from "react";
+
+
+const Part1=React.lazy(()=>import("./components/Layout/Part1/Part1_layout"));
+const Part2=React.lazy(()=>import("./components/Layout/Part2/Part2_layout"));
+const Part3=React.lazy(()=>import("./components/Layout/Part3/Part3"));
 
 
 function App() {
   return (
-    <Layout>
+    
+     <Layout>
+     <Suspense fallback={<p>Loading.....</p>}>
        <Routes>
        <Route path="/" element={<Navigate to={"/home"}/>}/>
         <Route path='/home' element={<Part1/>}/>
         <Route path='/skills' element={<Part2/>}/>
         <Route path='/about' element={<Part3/>}/>
         </Routes>
-    </Layout>
+    </Suspense>
+    </Layout> 
+   
   );
 }
 
