fc9cadcf4d5bb2d1558388df1efcf5fd5e9de5c8
Prabesh <prabesh.bista369@gmail.com>
2022-04-29 09:03:23 +0545

    .

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
-const container=document.getElementById('root');
-const root=createRoot(container);
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
-  <HashRouter>
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
-  </HashRouter>,
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
